411d9b2c05773b1fe18978207156c45739faa469
Anish Singh Walia <loveeanish@gmail.com>
2024-05-28 13:51:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,38 @@
-// app.js
 const express = require('express');
 const app = express();
 const getRedisClient = require('./redisClient');
 const client = getRedisClient();
 
-// Endpoint without caching
 app.get('/data', (req, res) => {
-  const data = fetchDataFromDB();
-  res.json(data);
+  try {
+    const data = fetchDataFromDB();
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: 'An error occurred fetching data from the DB' });
+  }
 });
 
-// Endpoint with Redis caching
 app.get('/cached-data', (req, res) => {
   const key = req.originalUrl;
   client.get(key, (err, data) => {
-    if (err) throw err;
+    if (err) {
+      res.status(500).json({ error: 'Redis error occurred' });
+      return;
+    }
     if (data !== null) {
       res.send(JSON.parse(data));
     } else {
-      const data = fetchCachedDataFromDB();
-      client.setex(key, 3600, JSON.stringify(data));
-      res.json(data);
+      try {
+        const data = fetchCachedDataFromDB();
+        client.setex(key, 3600, JSON.stringify(data));
+        res.json(data);
+      } catch (error) {
+        res.status(500).json({ error: 'An error occurred fetching cached data from the DB' });
+      }
     }
   });
 });
 
-// Simulate database calls
 const fetchDataFromDB = () => {
   return { data: 'This is data from the DB' };
 };
@@ -34,8 +41,6 @@ const fetchCachedDataFromDB = () => {
   return { data: 'This is Cached data from the DB' };
 };
 
-for (let line of startupMessage.split("\n")) {
-	console.log(line)
-}
-
-app.listen(port, () => console.log(`sample-expressjs app listening on port ${port}!`))
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
